4682c98a97d526906adcc965196da51cc7c66a95
maxattwell <max.attwell@hotmail.com>
2022-12-21 23:17:00 +1300

    removed background color

--- lua/max/colorscheme.lua
+++ lua/max/colorscheme.lua
@@ -5,3 +5,7 @@ if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
+-- Remove background color
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
